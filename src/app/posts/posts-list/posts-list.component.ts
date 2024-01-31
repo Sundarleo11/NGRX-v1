e6bc14cbc1491b1e1e54239be07b3c5e6e1a0239
Sundarleo11 <sundarrock1999@gmail.com>
2024-01-31 21:12:03 +0530

    updated selector

--- src/app/posts/posts-list/posts-list.component.ts
+++ src/app/posts/posts-list/posts-list.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Store } from "@ngrx/store";
+import { Observable } from "rxjs";
+import { appState } from "src/app/Appstore/app.store";
+import { Post } from "src/app/model/posts.model";
+import { getPosts } from "../State/posts.selector";
 
 @Component({
-  selector: 'app-posts-list',
-  templateUrl: './posts-list.component.html',
-  styleUrls: ['./posts-list.component.css']
+  selector: "app-posts-list",
+  templateUrl: "./posts-list.component.html",
+  styleUrls: ["./posts-list.component.css"],
 })
 export class PostsListComponent implements OnInit {
-
-  constructor() { }
+  posts: Observable<Post[]>;
+  constructor(private store: Store<appState>) {}
 
   ngOnInit() {
+    this.start();
   }
 
+  private start() {
+    this.posts = this.store.select(getPosts);
+  }
 }
