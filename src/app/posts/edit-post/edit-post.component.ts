98736f92987499b44c087caf26d8842f5578e52d
Sundarleo11 <sundarrock1999@gmail.com>
2024-02-03 20:25:41 +0530

    add router edit post

--- src/app/posts/edit-post/edit-post.component.ts
+++ src/app/posts/edit-post/edit-post.component.ts
@@ -1,10 +1,12 @@
+import { Route } from "@angular/compiler/src/core";
 import { Component, OnInit } from "@angular/core";
 import { FormControl, FormGroup, Validators } from "@angular/forms";
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
 import { Store } from "@ngrx/store";
 import { Observable } from "rxjs";
 import { appState } from "src/app/Appstore/app.store";
 import { Post } from "src/app/model/posts.model";
+import { updatePost } from "../State/posts.action";
 import { getPostById } from "../State/posts.selector";
 
 @Component({
@@ -16,14 +18,18 @@ export class EditPostComponent implements OnInit {
   post: Post;
   postDetails$: Observable<Post>;
   postForm: FormGroup;
-  constructor(private router: ActivatedRoute, private store: Store<appState>) {}
+  constructor(
+    private route: ActivatedRoute,
+    private store: Store<appState>,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     this.onload();
   }
 
   private onload() {
-    this.router.paramMap.subscribe((param) => {
+    this.route.paramMap.subscribe((param) => {
       const id = param.get("id");
 
       this.store.select(getPostById, { id }).subscribe((data) => {
@@ -37,13 +43,29 @@ export class EditPostComponent implements OnInit {
     this.postForm = new FormGroup({
       title: new FormControl(this.post.title, [
         Validators.required,
-        Validators.maxLength(6),
+        Validators.minLength(6),
       ]),
 
       decription: new FormControl(this.post.decription, [
         Validators.required,
-        Validators.maxLength(10),
+        Validators.minLength(25),
       ]),
     });
+  }
+
+  onUpdate() {
+    console.log(this.postForm);
+
+    const title = this.postForm.value.title;
+    const decription = this.postForm.value.decription;
+
+    const post: Post = {
+      id: this.post.id,
+      title,
+      decription,
+    };
+
+    this.store.dispatch(updatePost({ post }));
+    this.router.navigate(["posts"]);
   }
 }
