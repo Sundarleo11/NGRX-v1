67c7e56c3773e231d146244fcd29e0a23850c7bb
Sundarleo11 <sundarrock1999@gmail.com>
2024-02-04 16:33:27 +0530

    updated auth state

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { ReactiveFormsModule } from "@angular/forms";
 import { Routes, RouterModule } from "@angular/router";
 import { LoginComponent } from "./login/login.component";
-
+import { AuthReducer } from "./login/State/auth.reducer";
+import { StoreModule } from "@ngrx/store";
+import { AUTH_STATE } from "../auth/login/State/auth.selector";
 const routes: Routes = [
   {
     path: "",
@@ -16,6 +18,11 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule, ReactiveFormsModule, RouterModule.forChild(routes)],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    RouterModule.forChild(routes),
+    StoreModule.forFeature(AUTH_STATE, AuthReducer),
+  ],
 })
 export class AuthModule {}
