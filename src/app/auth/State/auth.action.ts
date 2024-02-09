6171eda85b0c21b084b0b9e4a59af7450e3527ac
Sundarleo11 <sundarrock1999@gmail.com>
2024-02-09 09:07:36 +0530

    created signup form

--- src/app/auth/State/auth.action.ts
+++ src/app/auth/State/auth.action.ts
@@ -1,8 +1,9 @@
 import { createAction, props } from "@ngrx/store";
 import { User } from "src/app/model/user.model";
-
 export const LOGIN_START = "[auth page] login start";
+export const SIGNUP_START = "[auth page] signup start";
 export const LOGIN_SUCCESS = "[auth page] login success";
+export const SIGNUP_SUCCESS = "[auth page] signup success";
 export const LOGIN_FAIL = "[auth page] login fail";
 
 export const loginStart = createAction(
@@ -12,5 +13,15 @@ export const loginStart = createAction(
 
 export const loginsuccess = createAction(
   LOGIN_SUCCESS,
+  props<{ user: User }>()
+);
+
+export const SignupStart = createAction(
+  SIGNUP_START,
+  props<{ email: string; password: string }>()
+);
+
+export const signupsuccess = createAction(
+  SIGNUP_SUCCESS,
   props<{ user: User }>()
 );
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -6,18 +6,20 @@ import { LoginComponent } from "./login/login.component";
 import { StoreModule } from "@ngrx/store";
 import { EffectsModule } from "@ngrx/effects";
 import { AuthEffects } from "./State/auth_effects";
+import { SignupComponent } from "./signup/signup.component";
 const routes: Routes = [
   {
     path: "",
     children: [
       { path: "", redirectTo: "login" },
       { path: "login", component: LoginComponent },
+      { path: "signup", component: SignupComponent },
     ],
   },
 ];
 
 @NgModule({
-  declarations: [LoginComponent],
+  declarations: [LoginComponent, SignupComponent],
   imports: [
     CommonModule,
     ReactiveFormsModule,
